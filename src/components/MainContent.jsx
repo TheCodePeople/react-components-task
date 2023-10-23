3f579f3ab254ae6e4b50ac00892218ace7d6c300
AlhassanAli01 <glasseyes47@gmail.com>
2023-10-23 16:20:34 +0300

    Add solution

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+const MainContent = () => {
+  return (
+    <main className="main-content">
+      <section className="section">
+        <h2>Section 1</h2>
+        <p>This is the first section of the webpage.</p>
+      </section>
+      <section className="section">
+        <h2>Section 2</h2>
+        <p>This is the second section of the webpage.</p>
+      </section>
+    </main>
+  );
+};
+
+export default MainContent;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>{/* App Component Here */}</React.StrictMode>);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
