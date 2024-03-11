d8458aebea4c6946d6a9f94397f80499b2fd0381
KJ-MU <gullssea5@gmail.com>
2024-03-11 18:12:44 +0300

    task complete

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <div id="footer">
+      <p>Cpoy Right @2024</p>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/components/MainContainer.js
@@ -0,0 +1,17 @@
+function MainContainer() {
+  return (
+    <div id="img-container">
+      <img
+        className="imges"
+        src="https://img.freepik.com/free-vector/realistic-black-shimmer-background_23-2150060287.jpg?w=996&t=st=1710167934~exp=1710168534~hmac=a80b6c89d58f89a42bd49a03d7ee7782823e894489f4b5d74aaf198ee57384ec"
+        alt="sparkels-img"
+      />
+      <img
+        className="imges"
+        src="https://i.pinimg.com/564x/92/6a/44/926a44c38c195bdabb29bea6bbdc1724.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+export default MainContainer;
